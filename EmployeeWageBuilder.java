a30fa6731ce8bb8c33a9d7d64c8db70080151750
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-20 18:34:04 +0530

    [amulya] added welcome message

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,13 @@
+package com.empwage;
+
+
+
+public class EmployeeWageBuilder {
+	static void welcome() { 
+		System.out.println("Welcome to Employee Wage Computation");
+	}
+	public static void main(String[] args) {
+		welcome();
+	}
+
+}
