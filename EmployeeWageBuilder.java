43d71861f2d8f630c975e0b92fd541e5bb6cf4fa
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-20 19:03:10 +0530

    [amulya] added daily wage of part time employee

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,27 +3,29 @@ package com.empwage;
 
 
 class Attendance{
-	public static int attendance() {
+	public static void attendance() {
 		int att=(int) Math.floor(Math.random()*2);
 		if(att==0) {
 			System.out.println("Employee is absent");
-			return 0;
 		}else {
 			System.out.println("Employee is present");
-			return 1;
 		}
 	}
 }
 class DailyWage extends EmployeeWageBuilder{
 	public static int fulltimeWage;
+	public static int parttimeWage;
 	public static void wage_calculator() {
 		 fulltimeWage=wage_per_hr*fulltime_hrs;
-		 System.out.println("Daily wage of an emmployee="+fulltimeWage);
+		 parttimeWage=wage_per_hr*parttime_hrs;
+		 System.out.println("Daily wage of a fulltime emmployee="+fulltimeWage);
+		 System.out.println("Daily wage of part-time emmployee="+parttimeWage);
 	}
 }
 public class EmployeeWageBuilder {
 	final static int wage_per_hr=20;
 	final static int fulltime_hrs=8;
+	final static int parttime_hrs=4;
 	static void welcome() { 
 		System.out.println("Welcome to Employee Wage Computation");
 	}
@@ -31,7 +33,6 @@ public class EmployeeWageBuilder {
 		welcome();
 		Attendance.attendance();
 		DailyWage.wage_calculator();
-		
 	}
 
 }
