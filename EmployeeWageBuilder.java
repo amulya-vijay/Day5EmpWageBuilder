6433584a23809955d266e9cdbfcf72cae0aa47bb
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-20 18:53:38 +0530

    [amulya] added daily wage of fulltime employee

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,5 +1,7 @@
 package com.empwage;
 
+
+
 class Attendance{
 	public static int attendance() {
 		int att=(int) Math.floor(Math.random()*2);
@@ -12,13 +14,24 @@ class Attendance{
 		}
 	}
 }
+class DailyWage extends EmployeeWageBuilder{
+	public static int fulltimeWage;
+	public static void wage_calculator() {
+		 fulltimeWage=wage_per_hr*fulltime_hrs;
+		 System.out.println("Daily wage of an emmployee="+fulltimeWage);
+	}
+}
 public class EmployeeWageBuilder {
+	final static int wage_per_hr=20;
+	final static int fulltime_hrs=8;
 	static void welcome() { 
 		System.out.println("Welcome to Employee Wage Computation");
 	}
 	public static void main(String[] args) {
 		welcome();
 		Attendance.attendance();
+		DailyWage.wage_calculator();
+		
 	}
 
 }
